84151bbb8bbf4c447a3bf3ca3b7f51b3010588f3
Thiago Turim Carvalho <turimthiago@gmail.com>
2025-02-19 11:15:13 -0300

    test: ensure call jwt.sign with correct values

--- tests/services/api-token/auth-api-token-credentials.test.ts
+++ tests/services/api-token/auth-api-token-credentials.test.ts
@@ -1,7 +1,11 @@
 import { Connection } from '../../../src/database/connection';
 import { UserCredentials } from '../../../src/models/user-credentials';
 import { AuthApiTokenCredentials } from '../../../src/services/api-token/auth-api-token-credentials';
+
+import jwt from 'jsonwebtoken';
 import { mock, MockProxy } from 'jest-mock-extended';
+
+jest.mock('jsonwebtoken');
 
 describe('AuthApiTokenCredentials', () => {
   let id: number;
@@ -9,15 +13,16 @@ describe('AuthApiTokenCredentials', () => {
   let password: string;
   let secretKey: string;
   let expirationInSeconds: number;
+  let fakeJwt: jest.Mocked<typeof jwt>;
   let connection: MockProxy<Connection>;
   let sut: AuthApiTokenCredentials;
 
   beforeAll(() => {
-	id = 112233;
+    id = 112233;
     username = 'any_username';
     password = 'any_p455w0rd';
-	secretKey = 'any_secretkey';
-	expirationInSeconds = 1;
+    secretKey = 'any_secretkey';
+    expirationInSeconds = 1;
     connection = mock<Connection>();
     connection.findUsernameAndPassword.mockResolvedValue(
       new UserCredentials({
@@ -26,6 +31,7 @@ describe('AuthApiTokenCredentials', () => {
         id,
       })
     );
+    fakeJwt = jwt as jest.Mocked<typeof jwt>;
   });
 
   beforeEach(() => {
@@ -38,12 +44,22 @@ describe('AuthApiTokenCredentials', () => {
 
   it('should call connection with correct values', async () => {
     await sut.auth({ password, username });
-    expect(connection.findUsernameAndPassword).toHaveBeenCalledWith({username, password});
+    expect(connection.findUsernameAndPassword).toHaveBeenCalledWith({
+      username,
+      password,
+    });
   });
 
   it('should throw error id connection returns undefined', async () => {
     connection.findUsernameAndPassword.mockResolvedValueOnce(undefined);
     const promise = sut.auth({ password, username });
-    await expect(promise).rejects.toThrow();
+    expect(promise).rejects.toThrow();
+  });
+
+  it('should call jwt.sign with correct values', async () => {
+    await sut.auth({ password, username });
+    expect(jwt.sign).toHaveBeenCalledWith({ username }, secretKey, {
+      expiresIn: expirationInSeconds,
+    });
   });
 });
